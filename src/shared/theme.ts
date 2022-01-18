4291a77901a1a5f61b72edf49ee785d7f345bae5
Marcos Adriano <mdrick19@gmail.com>
2022-01-18 20:01:47 -0300

    chore: add theme metrics and colors

---
+++ src/shared/theme.ts
@@ -0,0 +1,18 @@
+export const metrics = {
+  halfSpace: 4,
+  space: 8,
+  doubleSpace: 16,
+  containerMargin: 20,
+  borderWidth: 1,
+};
+
+export const colors = {
+  primary: '#4A5C9A',
+  screenBackground: '#EEE',
+  componentBackground: '#FFF',
+  borderColor: '#EEE',
+  monochromatic1: '#000',
+  monochromatic2: '#4D5A60',
+};
+
+export const theme = {metrics: {...metrics}, colors: {...colors}};
