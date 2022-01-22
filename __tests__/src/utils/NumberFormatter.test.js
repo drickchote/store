609be162e3cda7e8b899880bfac5f83afe848a33
Marcos Adriano <mdrick19@gmail.com>
2022-01-22 20:46:53 -0300

    test(utils/NumberFormatter): add formatStock function

--- __tests__/src/utils/NumberFormatter.test.js
+++ __tests__/src/utils/NumberFormatter.test.js
@@ -1,4 +1,4 @@
-import {toMoney} from '~/utils/NumberFormatter';
+import {formatStock, toMoney} from '~/utils/NumberFormatter';
 
 describe('toMoney function', () => {
   test('50.99 should return R$ 50,99', () => {
@@ -7,5 +7,15 @@ describe('toMoney function', () => {
 
   test('10000 should return R$ R$ 10.000,00', () => {
     expect(toMoney(10000)).toBe('R$ 10.000,00');
+  });
+});
+
+describe('formatStock function', () => {
+  it('should return same number when stock <= 999', () => {
+    expect(formatStock(999)).toBe('999');
+  });
+
+  it('should return 999+ when stock > 999', () => {
+    expect(formatStock(1000)).toBe('999+');
   });
 });
