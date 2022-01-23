ae879577626069bdff414b6786f68c897491b2da
Marcos Adriano <mdrick19@gmail.com>
2022-01-23 10:38:41 -0300

    test(components/Cart): add Cart component

---
+++ __tests__/src/components/Cart.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {Cart} from '~/components';
+import {formatCartNumber} from '~/utils/NumberFormatter';
+import {render} from '~/utils/test-utils';
+import {toHaveStyle} from '@testing-library/jest-native';
+import {colors} from '~/shared';
+
+describe('Render CartIcon properly', () => {
+  expect.extend({toHaveStyle});
+  it('should render without errors', () => {
+    const {getByText} = render(<Cart quantity={5} />);
+    const cart = getByText('5');
+    expect(cart).toBeDefined();
+  });
+
+  it('should format quantity', () => {
+    const quantity = 10;
+    const formatterQuantity = formatCartNumber(quantity);
+    const {getByText} = render(<Cart quantity={quantity} />);
+    const cart = getByText(formatterQuantity);
+    expect(cart).toBeDefined();
+  });
+
+  it('should not show circle when quantity is 0', () => {
+    const quantity = 0;
+    const formatterQuantity = formatCartNumber(quantity);
+    const {getByTestId} = render(<Cart quantity={quantity} />);
+    const cartQuantity = getByTestId('cartQuantity');
+    expect(formatterQuantity).toEqual('');
+    expect(cartQuantity).toHaveStyle({
+      backgroundColor: colors.screenBackground,
+    });
+  });
+});
