32fe0815a61643bcc14725e5f947f5f79e451ccb
Marcos Adriano <mdrick19@gmail.com>
2022-01-23 12:21:27 -0300

    test(components/SearchInput): add SearchInput

---
+++ __tests__/src/components/SearchInput.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {SearchInput} from '~/components';
+import {render} from '~/utils/test-utils';
+
+test('Render SearchInput without errors', () => {
+  render(<SearchInput />);
+});
