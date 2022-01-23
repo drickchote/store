0a913ccb7cc2ba57932f0d6b1dacf30853d97de3
Marcos Adriano <mdrick19@gmail.com>
2022-01-22 22:16:28 -0300

    task(shared/theme): add readonly property to theme constants

--- src/shared/theme.ts
+++ src/shared/theme.ts
@@ -4,7 +4,7 @@ export const metrics = {
   doubleSpace: 16,
   containerMargin: 20,
   borderWidth: 1,
-};
+} as const;
 
 export const colors = {
   primary: '#4A5C9A',
@@ -13,9 +13,12 @@ export const colors = {
   borderColor: '#EEE',
   monochromatic1: '#000',
   monochromatic2: '#4D5A60',
-};
+} as const;
 
-export const theme = {metrics: {...metrics}, colors: {...colors}};
+export const theme = {
+  metrics: {...metrics} as const,
+  colors: {...colors} as const,
+};
 
 export interface ThemeInterface {
   metrics: typeof metrics;
