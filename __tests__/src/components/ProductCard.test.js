87045d9f846b96772a3055338e39b6b65ea510a9
Marcos Adriano <mdrick19@gmail.com>
2022-01-22 22:23:56 -0300

    test(components/ProductCard): add ProductCard component

---
+++ __tests__/src/components/ProductCard.test.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import {ProductCard} from '~/components/ProductCard';
+import {render} from '~/utils/test-utils';
+import {toHaveStyle, toContainElement} from '@testing-library/jest-native';
+import {formatStock, toMoney} from '~/utils/NumberFormatter';
+
+describe('ProductCard', () => {
+  expect.extend({toHaveStyle, toContainElement});
+
+  describe('Render properly', () => {
+    it('render with no errors', () => {
+      const {getByTestId} = render(
+        <ProductCard
+          name="Rustic Metal Fish"
+          isSelected={false}
+          price={50}
+          stock={10}
+          uri="https://picsum.photos/640/480/"
+          handlePress={() => {}}
+        />,
+      );
+      const productCard = getByTestId('productCard');
+      expect(productCard).toBeDefined();
+    });
+
+    it('render formatted price', () => {
+      const price = 50;
+      const formattedPrice = toMoney(price);
+      const {getByText} = render(
+        <ProductCard
+          name="Rustic Metal Fish"
+          isSelected={false}
+          price={price}
+          stock={10}
+          uri="https://picsum.photos/640/480/"
+          handlePress={() => {}}
+        />,
+      );
+      const priceHeadind = getByText(formattedPrice);
+      expect(priceHeadind).toBeDefined();
+    });
+
+    it('render formatted stock', () => {
+      const stock = 1000;
+      const formattedStock = formatStock(stock);
+      const {getByText} = render(
+        <ProductCard
+          name="Rustic Metal Fish"
+          isSelected={false}
+          price={50}
+          stock={stock}
+          uri="https://picsum.photos/640/480/"
+          handlePress={() => {}}
+        />,
+      );
+      const stockText = `Disponível: ${formattedStock}`;
+      const stockHeadind = getByText(stockText);
+      expect(stockHeadind).toBeDefined();
+    });
+  });
+
+  describe('selected state', () => {
+    it('should not show icon when not selected', () => {
+      const {queryByTestId} = render(
+        <ProductCard
+          name="Rustic Metal Fish"
+          isSelected={false}
+          price={50}
+          stock={10}
+          uri="https://picsum.photos/640/480/"
+          handlePress={() => {}}
+        />,
+      );
+      const icon = queryByTestId('check-icon');
+      expect(icon).toBeNull();
+    });
+
+    it('should show icon when selected', () => {
+      const {getByTestId} = render(
+        <ProductCard
+          name="Rustic Metal Fish"
+          isSelected={true}
+          price={50}
+          stock={10}
+          uri="https://picsum.photos/640/480/"
+          handlePress={() => {}}
+        />,
+      );
+      const productCard = getByTestId('productCard');
+      const icon = getByTestId('check-icon');
+
+      expect(productCard).toContainElement(icon);
+    });
+
+    it('should show border when selected', () => {
+      const {getByTestId} = render(
+        <ProductCard
+          name="Rustic Metal Fish"
+          isSelected={true}
+          price={50}
+          stock={10}
+          uri="https://picsum.photos/640/480/"
+          handlePress={() => {}}
+        />,
+      );
+      const productCard = getByTestId('productCard');
+      expect(productCard).toHaveStyle({borderWidth: 3});
+    });
+  });
+});
